fb20831ce6376dc5c35f097752a1c0b22e4a8f7c
Scott Lowe <scottwithtwotees@gmail.com>
2011-11-13 13:52:34 +0000

    Add constant-to-command dispatch translation because some constants map to the same command; Add L-system definitions for Sierpinski triangle and dragon curve.

--- src/l_system/core.clj
+++ src/l_system/core.clj
@@ -1,25 +1,5 @@
 (ns l-system.core)
 
-(defprotocol StackOps
-  (push-stack [this x])
-  (pop-stack [this])
-  (top-stack [this])
-  (empty-stack? [this]))
-
-(defrecord Stack [elements]
-  StackOps
-  (push-stack [_ x]
-    (dosync (alter elements conj x)))
-  (pop-stack [_]
-    (let [fst (first @elements)]
-      (dosync (alter elements rest)) fst))
-  (top-stack [_]
-    (first @elements))
-  (empty-stack? [_]
-    (= () @elements)))
-
-(defn new-stack [elements] (new Stack elements))
-
 (defn apply-rules
   "Applies production rules to a sequence of characters for a given grammer"
   [grammar pattern]
@@ -35,28 +15,65 @@
     n))
 
 
-;; grammar definitions from here on
+;; grammar definitions
 
-(def tree-a
-  {:constants #{\F \+ \- \[ \]}
-   :start      [\F]
-   :rules      {\F "F[+F]F[-F]F"}
-   :commands   {\F :forward
-                \+ :left
-                \- :right
-                \[ :push
-                \] :pop
-                }
-   :angle       25.7})
+(def tree-commands {\F :forward
+                    \+ :left
+                    \- :right
+                    \[ :push
+                    \] :pop})
 
-(def tree-f
-  {:constants #{\F \+ \- \[ \]}
-   :start      [\X]
-   :rules      {\X "F-[[X]+X]+F[+FX]-X"
-                \F "FF"}
-   :commands   {\F :forward
-                \+ :left
-                \- :right
-                \[ :push
-                \] :pop}
-   :angle       22.5})
+(def axial-tree-a {:start     [\F]
+                   :rules     {\F "F[+F]F[-F]F"}
+                   :angle     25.7
+                   :constants #{\F \+ \- \[ \]}
+                   :commands  tree-commands })
+
+(def axial-tree-f {:start     [\X]
+                   :rules     {\X "F-[[X]+X]+F[+FX]-X"
+                               \F "FF"}
+                   :angle     22.5
+                   :constants #{\F \+ \- \[ \]}
+                   :commands  tree-commands})
+
+(def dragon-curve {:constants  #{\F \+ \-}
+                   :start      [\F \X]
+                   :rules      {\X "X+YF"
+                                \Y "FX-Y"}
+                   :angle       90
+                   :commands   {\F :forward
+                                \+ :left
+                                \- :right}})
+
+(def sierpinski-triangle {:constants  #{\+ \-}
+                          :start      [\A]
+                          :rules      {\A "B-A-B"
+                                       \B "A+B+A"}
+                          :angle       60
+                          :commands   {\A :forward
+                                       \B :forward
+                                       \+ :left
+                                       \- :right}})
+
+
+;; mutable stack implementation
+
+(defprotocol IStack
+  (push-stack [this x])
+  (pop-stack [this])
+  (top-stack [this])
+  (empty-stack? [this]))
+
+(defrecord Stack [elements]
+  IStack
+  (push-stack [_ x]
+    (dosync (alter elements conj x)))
+  (pop-stack [_]
+    (let [fst (first @elements)]
+      (dosync (alter elements rest)) fst))
+  (top-stack [_]
+    (first @elements))
+  (empty-stack? [_]
+    (= () @elements)))
+
+(defn new-stack [elements] (new Stack elements))
--- src/l_system/display.clj
+++ src/l_system/display.clj
@@ -28,17 +28,18 @@
 (defn plot-system [grammar params]
   (let [turtle (atom {:x (first (:origin params))
                       :y (second (:origin params))
-                      :angle 180})
+                      :angle (:start-angle params)})
         angle  (:angle grammar)
         length (:line-length params)
+        commands (:commands grammar)
         stack  (new-stack (ref ()))
         dispatch (fn [constant]
                    (cond
-                     (= \F constant) (forward turtle length)
-                     (= \+ constant) (turn turtle + angle)
-                     (= \- constant) (turn turtle - angle)
-                     (= \[ constant) (push-stack stack @turtle)
-                     (= \] constant) (swap! turtle #(do % (pop-stack stack)))))]
+                     (= :left    (commands constant)) (turn turtle + angle)
+                     (= :push    (commands constant)) (push-stack stack @turtle)
+                     (= :pop     (commands constant)) (swap! turtle #(do % (pop-stack stack)))
+                     (= :right   (commands constant)) (turn turtle - angle)
+                     (= :forward (commands constant)) (forward turtle length)))]
     (dorun
       (map dispatch
            (seq (generate grammar (:n-productions params)))))))
@@ -54,26 +55,32 @@
   (plot-system grammar params))
 
 (defapplet tree-a-app
- :title "Axial Tree A"
- :setup #(do (init tree-a {:origin [300 800]
-                           :n-productions 4
-                           :line-length 8}))
- :size [600 800])
+  :title "Axial Tree A"
+  :size [400 670]
+  :setup #(do (init axial-tree-a {:origin [200 670]
+                                  :n-productions 4
+                                  :line-length 8
+                                  :start-angle 180})))
 
 (defapplet tree-f-app
- :title "Axial Tree F"
- :setup #(do (init tree-f {:origin [370 800]
-                           :n-productions 5
-                           :line-length 9}))
- :size [650 800])
+  :title "Axial Tree F"
+  :size [600 700]
+  :setup #(do (init axial-tree-f {:origin [350 700]
+                                  :n-productions 5
+                                  :line-length 8
+                                  :start-angle 180})))
 
-
-;(defapplet sierpinski-app
-;  :title "L-system Sierpinski Triangle"
-;  :setup #(do (setup {:x 80 :y 234} sierpinski-triangle 6))
-;  :size [600 480])
-;
-;(defapplet dragon-curve-app
-;  :title "L-system Dragon Curve"
-;  :setup #(do (setup {:x 350 :y 350} dragon-curve 11))
-;  :size [600 480])
+(defapplet sierpinski-app
+  :title "Sierpinski Triangle"
+  :size [700 600]
+  :setup #(do (init sierpinski-triangle {:origin [20 20]
+                                         :n-productions 7
+                                         :line-length 5
+                                         :start-angle 90})))
+(defapplet dragon-app
+  :title "Dragon Curve"
+  :size [630 470]
+  :setup #(do (init dragon-curve {:origin [470 280]
+                                  :n-productions 12
+                                  :line-length 6
+                                  :start-angle 90})))
