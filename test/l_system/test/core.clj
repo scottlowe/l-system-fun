93afa6ebb1dabdd105f803c0004fc1703a3491fb
Scott Lowe <scottwithtwotees@gmail.com>
2012-03-02 09:55:15 +1100

    Add more tests for generated strings

--- test/l_system/test/core.clj
+++ test/l_system/test/core.clj
@@ -1,11 +1,23 @@
 (ns l-system.test.core
   (:use [l-system.core]
+        [l-system.grammars]
         [clojure.test]))
 
 
-(deftest generate-tree-a-test
+(deftest generate-axial-tree-a
   (is (= "F[+F]F[-F]F"
-         (generate tree-a 1)))
+         (generate axial-tree-a 1)))
   (is (= "F[+F]F[-F]F[+F[+F]F[-F]F]F[+F]F[-F]F[-F[+F]F[-F]F]F[+F]F[-F]F"
-         (generate tree-a 2))))
+         (generate axial-tree-a 2))))
 
+(deftest generate-dragon-curve
+  (is (= "FX+YF"
+         (generate dragon-curve 1)))
+  (is (= "FX+YF+FX-YF+FX+YF-FX-YF+FX+YF+FX-YF-FX+YF-FX-YF"
+         (generate dragon-curve 4))))
+
+(deftest generate-sierpinski-triangle
+  (is (= "A+B+A-B-A-B-A+B+A"
+         (generate sierpinski-triangle 2)))
+  (is (= "B-A-B+A+B+A+B-A-B-A+B+A-B-A-B-A+B+A-B-A-B+A+B+A+B-A-B"
+         (generate sierpinski-triangle 3))))
