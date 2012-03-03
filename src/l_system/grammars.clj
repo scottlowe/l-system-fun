eee353b0100e33337c8d956b497c170e89afe207
Scott Lowe <scottwithtwotees@gmail.com>
2012-03-03 13:45:23 +1100

    Rework to separate concerns. Line coordinates are now calculated separately from drawing step.

--- src/l_system/grammars.clj
+++ src/l_system/grammars.clj
@@ -1,47 +1,47 @@
 (ns l-system.grammars)
 
-(def tree-commands {\F :forward
-                    \+ :left
-                    \- :right
-                    \[ :push
-                    \] :pop})
+(def tree-cmd-map {\F :forward
+                   \+ :left
+                   \- :right
+                   \[ :push
+                   \] :pop})
 
 (def axial-tree-a {:start     [\F]
                    :rules     {\F "F[+F]F[-F]F"}
                    :angle     25.7
                    :constants #{\F \+ \- \[ \]}
-                   :commands  tree-commands })
+                   :cmd-map   tree-cmd-map})
 
 (def axial-tree-d {:start     [\X]
                    :rules     {\X "F[+X]F[-X]+X"
                                \F "FF"}
                    :angle     20
                    :constants #{\F \+ \- \[ \]}
-                   :commands  tree-commands})
+                   :cmd-map   tree-cmd-map})
 
 (def axial-tree-f {:start     [\X]
                    :rules     {\X "F-[[X]+X]+F[+FX]-X"
                                \F "FF"}
                    :angle     22.5
                    :constants #{\F \+ \- \[ \]}
-                   :commands  tree-commands})
+                   :cmd-map   tree-cmd-map})
 
-(def dragon-curve {:constants  #{\F \+ \-}
-                   :start      [\F \X]
-                   :rules      {\X "X+YF"
-                                \Y "FX-Y"}
-                   :angle       90
-                   :commands   {\F :forward
-                                \+ :left
-                                \- :right}})
+(def dragon-curve {:constants #{\F \+ \-}
+                   :start     [\F \X]
+                   :rules     {\X "X+YF"
+                               \Y "FX-Y"}
+                   :angle      90
+                   :cmd-map   {\F :forward
+                               \+ :left
+                               \- :right}})
 
-(def sierpinski-triangle {:constants  #{\+ \-}
-                          :start      [\A]
-                          :rules      {\A "B-A-B"
-                                       \B "A+B+A"}
-                          :angle       60
-                          :commands   {\A :forward
-                                       \B :forward
-                                       \+ :left
-                                       \- :right}})
+(def sierpinski-triangle {:constants #{\+ \-}
+                          :start     [\A]
+                          :rules     {\A "B-A-B"
+                                      \B "A+B+A"}
+                          :angle      60
+                          :cmd-map   {\A :forward
+                                      \B :forward
+                                      \+ :left
+                                      \- :right}})
 
--- test/l_system/test/core.clj
+++ test/l_system/test/core.clj
@@ -3,21 +3,32 @@
         [l-system.grammars]
         [clojure.test]))
 
-
 (deftest generate-axial-tree-a
-  (is (= "F[+F]F[-F]F"
-         (generate axial-tree-a 1)))
-  (is (= "F[+F]F[-F]F[+F[+F]F[-F]F]F[+F]F[-F]F[-F[+F]F[-F]F]F[+F]F[-F]F"
-         (generate axial-tree-a 2))))
+         (is (= "F[+F]F[-F]F"
+                (generate axial-tree-a 1)))
+         (is (= "F[+F]F[-F]F[+F[+F]F[-F]F]F[+F]F[-F]F[-F[+F]F[-F]F]F[+F]F[-F]F"
+                (generate axial-tree-a 2))))
 
 (deftest generate-dragon-curve
-  (is (= "FX+YF"
-         (generate dragon-curve 1)))
-  (is (= "FX+YF+FX-YF+FX+YF-FX-YF+FX+YF+FX-YF-FX+YF-FX-YF"
-         (generate dragon-curve 4))))
+         (is (= "FX+YF"
+                (generate dragon-curve 1)))
+         (is (= "FX+YF+FX-YF+FX+YF-FX-YF+FX+YF+FX-YF-FX+YF-FX-YF"
+                (generate dragon-curve 4))))
 
 (deftest generate-sierpinski-triangle
-  (is (= "A+B+A-B-A-B-A+B+A"
-         (generate sierpinski-triangle 2)))
-  (is (= "B-A-B+A+B+A+B-A-B-A+B+A-B-A-B-A+B+A-B-A-B+A+B+A+B-A-B"
-         (generate sierpinski-triangle 3))))
+         (is (= "A+B+A-B-A-B-A+B+A"
+                (generate sierpinski-triangle 2)))
+         (is (= "B-A-B+A+B+A+B-A-B-A+B+A-B-A-B-A+B+A-B-A-B+A+B+A+B-A-B"
+                (generate sierpinski-triangle 3))))
+
+(deftest gen-coords-test
+         (is (= [[200 670 200.0 662.0]
+                 [200.0 662.0 196.53072732329966 654.7913838294232]
+                 [200.0 662.0 200.0 654.0]
+                 [200.0 654.0 203.46927267670034 646.7913838294232]
+                 [200.0 654.0 200.0 646.0]]
+                (gen-coords axial-tree-a
+                            {:origin [200 670]
+                             :n-productions 1
+                             :line-length 8
+                             :start-angle 180}))))
