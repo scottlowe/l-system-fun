dfcb5e4c278504b7979fb70ec2c4d195c68f3689
Scott Lowe <scottwithtwotees@gmail.com>
2012-04-29 03:24:22 +0100

    Refactor and refine: It's true, you really can replace most loops with a HOF

--- src/l_system/core.clj
+++ src/l_system/core.clj
@@ -3,10 +3,10 @@
 
 (def ^:dynamic *grammar* axial-tree-a)
 
-(def ^:dynamic *params* {:origin [200 200]
-                         :n-productions 4
-                         :line-length 7
-                         :start-angle 180})
+(def ^:dynamic *environment* {:origin [200 200]
+                              :n-productions 4
+                              :line-length 7
+                              :start-angle 180})
 
 (defn apply-rules [grammar pattern]
   (apply str
@@ -18,7 +18,7 @@
       (partial apply-rules grammar) (:start grammar))
     n))
 
-(defn line-end [turtle length]
+(defn- new-position [turtle length]
   (let [deg   (/ (Math/PI) 180)
         angle (:angle turtle)]
     {:angle angle
@@ -33,42 +33,38 @@
   (assoc turtle :angle
          (direction (:angle turtle) angle)))
 
-(defn exec-cmd [turtle cmd stack lines]
-  (let [angle  (:angle *grammar*)
-        length (:line-length *params*)]
+(defn- exec-cmd [state command]
+  "For a given command and state, returns a new state consisting of a turtle
+  position, line drawing coordinates & a FIFO stack of turtle positions"
+  (let [angle (:angle *grammar*)
+        length (:line-length *environment*)
+        cmd ((:cmd-map *grammar*) command)
+        {turtle :turtle stack :stack} state]
     (cond
-      (= :left cmd)    [(turn turtle + angle) stack lines]
-      (= :push cmd)    [turtle (cons turtle stack) lines]
-      (= :pop cmd)     [(first stack) (rest stack) lines]
-      (= :right cmd)   [(turn turtle - angle) stack lines]
-      (= :forward cmd) (do
-                         (let [new-turtle (line-end turtle length)
-                               {x1 :x y1 :y} turtle
-                               {x2 :x y2 :y} new-turtle
-                               new-lines (conj lines [x1 y1 x2 y2])]
-                           [new-turtle stack new-lines]))
-      :else [turtle stack lines])))
+      (= :left cmd)    (assoc state :turtle (turn turtle + angle))
+      (= :push cmd)    (assoc state :stack (cons turtle stack))
+      (= :pop cmd)     (assoc state :turtle (first stack) :stack (rest stack))
+      (= :right cmd)   (assoc state :turtle (turn turtle - angle))
+      (= :forward cmd) (let [new-turtle (new-position turtle length)
+                             {x1 :x y1 :y} turtle
+                             {x2 :x y2 :y} new-turtle
+                             new-lines (conj (:lines state) [x1 y1 x2 y2])]
+                         (assoc state :turtle new-turtle :lines new-lines))
+      :else state)))
 
-(defn calc-moves [start-pos commands]
-  (loop [turtle start-pos
-         cmds commands
-         stack '()
-         line-coords []]
-    (if (empty? cmds)
-      line-coords
-      (let [[new-turtle
-             new-stack
-             new-lines] (exec-cmd turtle
-                                  ((:cmd-map *grammar*) (first cmds))
-                                  stack
-                                  line-coords)]
-        (recur new-turtle (rest cmds) new-stack new-lines)))))
+(defn- calc-moves [turtle-start commands]
+  "Calculates line drawing coordinates for a collection of l-system commands"
+  (let [state {:turtle turtle-start :stack '() :lines []}]
+    (:lines
+      (reduce exec-cmd state commands))))
 
-(defn gen-coords [grammar params]
+(defn gen-coords [grammar environment]
+  "Generates drawing coordinates for an l-system grammar and start conditions"
   (binding [*grammar* grammar
-            *params*  params]
-    (let [turtle {:x (first (:origin params))
-                  :y (second (:origin params))
-                  :angle (:start-angle params)}]
+            *environment*  environment]
+    (let [turtle {:x (first (:origin environment))
+                  :y (second (:origin environment))
+                  :angle (:start-angle environment)}]
       (calc-moves turtle
-                  (gen-commands grammar (:n-productions params))))))
+                  (gen-commands grammar
+                                (:n-productions environment))))))
